28f1cac4e82df97710b8a9be1bf1cd0fa273b573
Juan Sebastian Herrera Gomez <86378159+JuanSHerrera1006@users.noreply.github.com>
2024-02-12 16:09:01 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Título y autor de la app
 st.title("Mi primera app")
-st.markdown("Esta app fue elaborada por *COLOQUE AQUÍ SU NOMBRE*.")
+st.markdown("Esta app fue elaborada por Juan Sebastian Herrera Gomez.")
 
 # Pregunta al usuario su nombre
 nombre_usuario = st.text_input("Por favor, ingresa tu nombre:")
